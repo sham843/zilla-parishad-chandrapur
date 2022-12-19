786bc7ac06832518af8e70b46032481bd9f40f19
Sanket <panchmind1@gmail.com>
2022-12-19 17:03:19 +0530

    student registration page design done.

--- src/app/modules/masters/student-registration/student-registration.module.ts
+++ src/app/modules/masters/student-registration/student-registration.module.ts
@@ -3,15 +3,32 @@ import { CommonModule } from '@angular/common';
 
 import { StudentRegistrationRoutingModule } from './student-registration-routing.module';
 import { StudentRegistrationComponent } from './student-registration.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import { RegisterStudentComponent } from './register-student/register-student.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
   declarations: [
-    StudentRegistrationComponent
+    StudentRegistrationComponent,
+    RegisterStudentComponent
   ],
   imports: [
     CommonModule,
-    StudentRegistrationRoutingModule
+    StudentRegistrationRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatMenuModule
+
   ]
 })
 export class StudentRegistrationModule { }
