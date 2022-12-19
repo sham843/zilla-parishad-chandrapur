827fb9e8dec538a0ab7a392fe57501eba36cc5d0
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-12-19 14:13:08 +0530

    new page components added

--- src/app/layouts/secure/secure-routing.module.ts
+++ src/app/layouts/secure/secure-routing.module.ts
@@ -4,7 +4,13 @@ import { SecureComponent } from './secure.component';
 
 const routes: Routes = [
   { path: '', component: SecureComponent },
-  { path: 'dashboard', loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule), data: { breadcrumb: [{ title: 'Dashboard', active: true }] } },];
+  { path: 'dashboard', loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule), data: { breadcrumb: [{ title: 'Dashboard', active: true }] } },
+  { path: 'designation-master', loadChildren: () => import('../../modules/masters/designation-master/designation-master.module').then(m => m.DesignationMasterModule), data: { breadcrumb: [{ title: 'Designation Master', active: true }] } },
+  { path: 'school-registration', loadChildren: () => import('../../modules/masters/school-registration/school-registration.module').then(m => m.SchoolRegistrationModule) },
+  { path: 'student-registration', loadChildren: () => import('../../modules/masters/student-registration/student-registration.module').then(m => m.StudentRegistrationModule) },
+  { path: 'user-registration', loadChildren: () => import('../../modules/masters/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
