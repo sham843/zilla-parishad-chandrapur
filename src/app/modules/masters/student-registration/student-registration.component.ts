0612bf8fa6d9af70d97904a70e0166231f2caa47
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2022-12-20 16:36:48 +0530

    commit changes

--- src/app/modules/masters/student-registration/student-registration.component.ts
+++ src/app/modules/masters/student-registration/student-registration.component.ts
@@ -16,51 +16,63 @@ export class StudentRegistrationComponent {
     private errors:ErrorsService) {}
 
     ngOnInit() {
-      // this.getTableData()
+      this.getTableData()
     }
 
-    // onPagintion(pageNo: number) {
-    //   this.pageNumber = pageNo;
-    //   this.getTableData()
-    // }
+    onPagintion(pageNo: number) {
+      this.pageNumber = pageNo;
+      this.getTableData()
+    }
   
     
-    // getTableData(flag?: string) {
-    //   this.pageNumber = flag == 'filter' ? 1 : this.pageNumber;
-    //   let tableDataArray = new Array();
-    //   let tableDatasize!: Number;
+    getTableData(flag?: string) {
+      this.pageNumber = flag == 'filter' ? 1 : this.pageNumber;
+      let tableDataArray = new Array();
+      let tableDatasize!: Number;
       
-    //   let str = `?pageno=${this.pageNumber}&pagesize=10&textSearch=`;
-    //   this.apiService.setHttp('GET', 'zp-Chandrapur/Student/GetAll' + str, false, false, false, 'baseUrl');
-    //   this.apiService.getHttp().subscribe({
+      let str = `?pageno=${this.pageNumber}&pagesize=10&textSearch=`;
+      this.apiService.setHttp('GET', 'zp-Chandrapur/Student/GetAll' + str, false, false, false, 'baseUrl');
+      this.apiService.getHttp().subscribe({
   
-    //     next: (res: any) => {
-    //       if (res.statusCode == "200") {
-    //         tableDataArray = res.responseData.responseData1;
-    //         tableDatasize = res.responseData.responseData2.pageCount;
-    //       } else {
-    //         tableDataArray = [];
-    //         tableDatasize = 0;
-    //       }
-    //       let displayedColumns = ['saralId', 'fullName', 'gender', 'standard', 'mobileNo', 'action'];
-    //       let displayedheaders = ['saralId', 'Name', 'Gender', 'Standard','Parents Contact No.','Action'];
-    //       let tableData = {
-    //         pageNumber: this.pageNumber,
-    //         img: '', blink: '', badge: '', isBlock: '', pagintion: true,
-    //         displayedColumns: displayedColumns, tableData: tableDataArray,
-    //         tableSize: tableDatasize,
-    //         tableHeaders: displayedheaders
-    //       };
-    //       this.apiService.tableData.next(tableData);
-    //     },
-    //     error: ((err: any) => { this.errors.handelError(err) })
-    //   });
-    // }
+        next: (res: any) => {
+          if (res.statusCode == "200") {
+            tableDataArray = res.responseData.responseData1;
+            tableDatasize = res.responseData.responseData2.pageCount;
+          } else {
+            tableDataArray = [];
+            tableDatasize = 0;
+          }
+          let displayedColumns = ['saralId', 'fullName', 'gender', 'standard', 'mobileNo', 'action'];
+          let displayedheaders = ['saralId', 'Name', 'Gender', 'Standard','Parents Contact No.','Action'];
+          let tableData = {
+            pageNumber: this.pageNumber,
+            img: '', blink: '', badge: '', isBlock: '', pagintion: true,
+            displayedColumns: displayedColumns, tableData: tableDataArray,
+            tableSize: tableDatasize,
+            tableHeaders: displayedheaders
+          };
+          this.apiService.tableData.next(tableData);
+        },
+        error: ((err: any) => { this.errors.handelError(err) })
+      });
+    }
 
 
   registerStudent(){
     this.dialog.open(RegisterStudentComponent, {
       width:'700px'
     });
+  }
+
+  childCompInfo(obj: any) {
+    switch (obj.label) {
+      case 'Pagination':
+        this.pageNumber = obj.pageNumber;
+        this.getTableData();
+        break;
+      case 'Edit' || 'Delete':
+        // this.addUpdateAgency(obj);
+        break;    
+    }
   }
 }
