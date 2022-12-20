c0f878aa648c7377d0bd93281fc636196c7674f2
Sanket <panchmind1@gmail.com>
2022-12-20 11:05:11 +0530

    page right acces page design done

--- src/app/modules/settings/page-right-access/page-right-access.module.ts
+++ src/app/modules/settings/page-right-access/page-right-access.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { PageRightAccessRoutingModule } from './page-right-access-routing.module';
 import { PageRightAccessComponent } from './page-right-access.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { PageRightAccessComponent } from './page-right-access.component';
   ],
   imports: [
     CommonModule,
-    PageRightAccessRoutingModule
+    PageRightAccessRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class PageRightAccessModule { }
