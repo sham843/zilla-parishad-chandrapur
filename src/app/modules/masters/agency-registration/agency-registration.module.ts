e4406748e9ce871b94fea9fefb532fca26fde76c
Sanket <panchmind1@gmail.com>
2022-12-21 14:01:25 +0530

    agency registration page design done.

--- src/app/modules/masters/agency-registration/agency-registration.module.ts
+++ src/app/modules/masters/agency-registration/agency-registration.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AgencyRegistrationRoutingModule } from './agency-registration-routing.module';
 import { AgencyRegistrationComponent } from './agency-registration.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { AgencyRegistrationComponent } from './agency-registration.component';
   ],
   imports: [
     CommonModule,
-    AgencyRegistrationRoutingModule
+    AgencyRegistrationRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class AgencyRegistrationModule { }
