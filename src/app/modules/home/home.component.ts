f9bed29c54b0c445b0cc8f4de44d9452e2af1694
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2022-12-22 17:49:14 +0530

    changes in home page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-constructor(){}
+constructor(private translate:TranslateService,
+  private webStorage:WebStorageService){
+    translate.addLangs(['English', 'Marathi'])
+    translate.setDefaultLang('English')
+  }
+  ngOnInit(){
+    let language: any = sessionStorage.getItem('language');
+    this.webStorage.setLanguage.next(language);
+    this.translate.use(language);
+  }
+
+onChangeLanguage(lang:any){
+  console.log(lang)
+  this.translate.use(lang)
+    this.webStorage.setLanguage.next(lang)
+    sessionStorage.setItem('language', lang)
+}
 }
