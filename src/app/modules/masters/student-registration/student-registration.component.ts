429699ca3af650efc3cf35b879ec7b170dd4b6a3
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2022-12-22 18:04:42 +0530

    student Reg upl

--- src/app/modules/masters/student-registration/student-registration.component.ts
+++ src/app/modules/masters/student-registration/student-registration.component.ts
@@ -1,11 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder,FormGroup, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ApiService } from 'src/app/core/services/api.service';
 import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
- import { ErrorsService } from 'src/app/core/services/errors.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
 import { ExcelPdfDownloadService } from 'src/app/core/services/excel-pdf-download.service';
- import { MasterService } from 'src/app/core/services/master.service';
+import { MasterService } from 'src/app/core/services/master.service';
 import { WebStorageService } from 'src/app/core/services/web-storage.service';
 import { GlobalDialogComponent } from 'src/app/shared/components/global-dialog/global-dialog.component';
 import { RegisterStudentComponent } from './register-student/register-student.component';
@@ -17,72 +17,72 @@ import { RegisterStudentComponent } from './register-student/register-student.co
   styleUrls: ['./student-registration.component.scss']
 })
 export class StudentRegistrationComponent {
- filterFrm!:FormGroup;
-  talukaArray=new Array();
-  centerArray=new Array();
-  schoolArray=new Array();
+  filterFrm!: FormGroup;
+  talukaArray = new Array();
+  centerArray = new Array();
+  schoolArray = new Array();
   pageNumber: number = 1;
   dataObj: any;
-  fname!:undefined;
-  lname!:undefined;
+  fname!: undefined;
+  lname!: undefined;
   lang: string | any = 'English';
   @ViewChild('formDirective')
-private formDirective!: NgForm;
-tableDataArray = new Array()
+  private formDirective!: NgForm;
+  tableDataArray = new Array()
 
   constructor(public dialog: MatDialog,
-    private webStorage:WebStorageService,
+    private webStorage: WebStorageService,
     private apiService: ApiService,
-   private commonMethod:CommonMethodsService,
-    private master:MasterService,
-    private errorService : ErrorsService,
-    private fb:FormBuilder,
+    private commonMethod: CommonMethodsService,
+    private master: MasterService,
+    private errorService: ErrorsService,
+    private fb: FormBuilder,
     private excelPdf: ExcelPdfDownloadService
   ) { }
 
   ngOnInit() {
-    this.webStorage.setLanguage.subscribe((res:any)=>{
-      this.lang=res
-      console.log("lannnnnn",this.lang)
+    this.webStorage.setLanguage.subscribe((res: any) => {
+      this.lang = res
+      // console.log("lannnnnn", this.lang)
     })
     this.formData();
     this.getTableData();
     this.getTaluka();
   }
-
-  formData(){
-    this.filterFrm=this.fb.group({
-      "talukaId":[0],
-      "centerId":[0],
-      "schoolId":[0],
-      "searchText":['']
-     })
+  //#region -----------------------------Filter Form Start-----------------------------------
+  formData() {
+    this.filterFrm = this.fb.group({
+      "talukaId": [0],
+      "centerId": [0],
+      "schoolId": [0],
+      "searchText": ['']
+    })
   }
+  //#endregion -----------------------------Filter Form End-----------------------------------
 
+  //#region -----------------------------Pagination Logic Start-----------------------------------
   onPagintion(pageNo: number) {
     this.pageNumber = pageNo;
     this.getTableData()
   }
+  //#endregion -----------------------------Pagination Logic End-----------------------------------
 
-
+  //#region -----------------------------Table Logic Start-----------------------------------
   getTableData(flag?: string) {
     this.pageNumber = flag == 'filter' ? 1 : this.pageNumber;
-   
     let tableDatasize!: Number;
-     let str = `?pageno=${this.pageNumber}&pagesize=10`;
-   
-  let formData=this.filterFrm.value;
-   this.apiService.setHttp('GET', 'zp-Chandrapur/Student/GetAll' + str + 
-   '&TalukaId='+(formData?.talukaId)+'&CenterId='+(formData?.centerId)
-   +'&SchoolId='+(formData?.centerId)+'&lan='+(this.lang)+'&searchText='+(formData?.searchText), false, false, false, 'baseUrl');
+    let str = `?pageno=${this.pageNumber}&pagesize=10`;
+    let formData = this.filterFrm.value;
+    this.apiService.setHttp('GET', 'zp-Chandrapur/Student/GetAll' + str +
+      '&TalukaId=' + (formData?.talukaId) + '&CenterId=' + (formData?.centerId)
+      + '&SchoolId=' + (formData?.centerId) + '&lan=' + (this.lang) + '&searchText=' + (formData?.searchText), false, false, false, 'baseUrl');
     this.apiService.getHttp().subscribe({
-
       next: (res: any) => {
         if (res.statusCode == "200") {
-         this.tableDataArray = res.responseData.responseData1;
-         this.tableDataArray.map((ele:any)=>{
-            ele.fullName = ele.f_Name + ' '+ele.m_Name+' '+ele.l_Name;
-           })
+          this.tableDataArray = res.responseData.responseData1;
+          this.tableDataArray.map((ele: any) => {
+            ele.fullName = ele.f_Name + ' ' + ele.m_Name + ' ' + ele.l_Name;
+          })
           tableDatasize = res.responseData.responseData2.pageCount;
         } else {
           this.tableDataArray = [];
@@ -93,7 +93,7 @@ tableDataArray = new Array()
         let tableData = {
           pageNumber: this.pageNumber,
           img: '', blink: '', badge: '', isBlock: '', pagintion: true,
-          displayedColumns: displayedColumns, tableData:this.tableDataArray,
+          displayedColumns: displayedColumns, tableData: this.tableDataArray,
           tableSize: tableDatasize,
           tableHeaders: displayedheaders,
           // edit:true,delete:true
@@ -103,8 +103,9 @@ tableDataArray = new Array()
       error: ((err: any) => { this.errorService.handelError(err) })
     });
   }
+//#endregion -----------------------------Table Logic End----------------------------------------
 
-
+//#region -----------------------------Add/Update Dialog Box Start---------------------------------
   registerStudent(obj?: any) {
     console.log("obj", obj)
     let dialogRef = this.dialog.open(RegisterStudentComponent, {
@@ -115,11 +116,10 @@ tableDataArray = new Array()
     dialogRef.afterClosed().subscribe((result: any) => {
       result == 'Yes' ? this.getTableData() : '';
     });
-
   }
-
- childCompInfo(obj: any) {
-  switch (obj.label) {
+//#endregion -----------------------------Add/Update Dialog Box End---------------------------------
+childCompInfo(obj: any) {
+    switch (obj.label) {
       case 'Pagination':
         this.pageNumber = obj.pageNumber;
         this.getTableData();
@@ -131,14 +131,14 @@ tableDataArray = new Array()
         this.globalDialogOpen(obj)
     }
   }
-
+//#region -----------------------------Delete Dialog Box Start---------------------------------
   globalDialogOpen(delObj?: any) {
     let dataObj = {
-     p1: 'Are you sure you want to delete this record?',
-       p2: '', cardTitle:  'Delete' ,
-        successBtnText:  'Delete',
-         dialogIcon: '', 
-         cancelBtnText: 'Cancel' 
+      p1: 'Are you sure you want to delete this record?',
+      p2: '', cardTitle: 'Delete',
+      successBtnText: 'Delete',
+      dialogIcon: '',
+      cancelBtnText: 'Cancel'
     }
     const dialogRef = this.dialog.open(GlobalDialogComponent, {
       width: '320px',
@@ -171,15 +171,16 @@ tableDataArray = new Array()
         }
       })
     });
-   
   }
+//#endregion -----------------------------Delete Dialog Box Start---------------------------------
 
+//#region--------------------------Filter Dropdown Start----------------------------------
   getTaluka() {
-   this.master.getAllTaluka(this.lang,1).subscribe({
+    this.master.getAllTaluka(this.lang, 1).subscribe({
       next: ((res: any) => {
         if (res.statusCode == "200") {
           this.talukaArray = res.responseData;
-          }
+        }
         else {
           this.talukaArray = [];
           this.commonMethod.checkEmptyData(res.statusMessage) == false ? this.errorService.handelError(res.statusCode) : this.commonMethod.snackBar(res.statusMessage, 1);
@@ -193,11 +194,11 @@ tableDataArray = new Array()
 
   getCenter() {
     let talukaId = this.filterFrm.value.talukaId;
-     this.master.getAllCenter(this.lang,talukaId).subscribe({
+    this.master.getAllCenter(this.lang, talukaId).subscribe({
       next: ((res: any) => {
         if (res.statusCode == "200") {
           this.centerArray = res.responseData;
-       }
+        }
         else {
           this.centerArray = [];
           this.commonMethod.checkEmptyData(res.statusMessage) == false ? this.errorService.handelError(res.statusCode) : this.commonMethod.snackBar(res.statusMessage, 1);
@@ -206,12 +207,12 @@ tableDataArray = new Array()
       error: (error: any) => {
         this.commonMethod.checkEmptyData(error.statusText) == false ? this.errorService.handelError(error.statusCode) : this.commonMethod.snackBar(error.statusText, 1);
       }
-    }) 
+    })
   }
 
   getSchool() {
     let centerId = this.filterFrm.value.centerId;
-    this.apiService.setHttp('GET', 'zp_chandrapur/master/GetAllSchoolsByCenter?flag_lang=' +  this.lang + '&CenterId=' + centerId, false, false, false, 'baseUrl');
+    this.apiService.setHttp('GET', 'zp_chandrapur/master/GetAllSchoolsByCenter?flag_lang=' + this.lang + '&CenterId=' + centerId, false, false, false, 'baseUrl');
     this.apiService.getHttp().subscribe({
       next: ((res: any) => {
         if (res.statusCode == "200") {
@@ -227,23 +228,22 @@ tableDataArray = new Array()
       }
     })
   }
+//#endregion--------------------------Filter Dropdown End----------------------------------
 
-
+//#region--------------------------Excel Download Logic Start----------------------------------
   excelDownload() {
-    let pageName='Student Registration';
-    let header=['Saral Id','Full Name','Taluka','Kendra','School Name'];
-    let column=['saralId', 'fullName','taluka','center','schoolName'];
-    this.excelPdf.downloadExcel(this.tableDataArray,pageName,header,column);
+    let pageName = 'Student Registration';
+    let header = ['Saral Id', 'Full Name', 'Taluka', 'Kendra', 'School Name'];
+    let column = ['saralId', 'fullName', 'taluka', 'center', 'schoolName'];
+    this.excelPdf.downloadExcel(this.tableDataArray, pageName, header, column);
   }
+//#endregion--------------------------Excel Download Logic End----------------------------------
 
-  clearForm() {
+//#region--------------------------Clear Form Logic Start---------------------------------- 
+clearForm() {
     this.formDirective && this.formDirective.resetForm();
     this.getTableData();
-   }
-
-
- 
-
-
+  }
+//#endregion--------------------------Clear Form Logic End---------------------------------- 
 }
 
