22fbae2797ba6aff0603a962291c7c772175e4d1
sham843 <sham.jadhav@shauryatechnosoft.com>
2022-12-23 09:50:23 +0530

    add expense guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
-// import { CheckLoggedInGuard } from './core/guards/check-logged-in.guard';
+import { CheckLoggedInGuard } from './core/guards/check-logged-in.guard';
 import { PublicComponent } from './layouts/public/public.component';
 import { SecureComponent } from './layouts/secure/secure.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', component: PublicComponent,
-  // canActivate: [CheckLoggedInGuard],
+  canActivate: [CheckLoggedInGuard],
   loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule) },
   {
     path: '',
@@ -17,7 +17,7 @@ const routes: Routes = [
     loadChildren: () => import('./layouts/secure/secure.module').then(m => m.SecureModule) 
   },
   
-  { path: '**', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule) },];
+  { path: '**', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule)}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layouts/secure/secure-routing.module.ts
+++ src/app/layouts/secure/secure-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpenseGuard } from 'src/app/core/guards/expense.guard';
 import { SecureComponent } from './secure.component';
 
 const routes: Routes = [
   { path: '', component: SecureComponent },
-  { path: 'dashboard', loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule), data: { breadcrumb: [{ title: 'Dashboard', active: true }] } },
-  { path: 'designation-master', loadChildren: () => import('../../modules/masters/designation-master/designation-master.module').then(m => m.DesignationMasterModule), data: { breadcrumb: [{ title: 'Designation Master', active: true }] } },
-  { path: 'school-registration', loadChildren: () => import('../../modules/masters/school-registration/school-registration.module').then(m => m.SchoolRegistrationModule), data: { breadcrumb: [{ title: 'School Registration', active: true }] }  },
-  { path: 'student-registration', loadChildren: () => import('../../modules/masters/student-registration/student-registration.module').then(m => m.StudentRegistrationModule), data: { breadcrumb: [{ title: 'Student Registration', active: true }] }  },
-  { path: 'user-registration', loadChildren: () => import('../../modules/masters/user-registration/user-registration.module').then(m => m.UserRegistrationModule), data: { breadcrumb: [{ title: 'User Registration', active: true }] }  },
-  { path: 'page-right-access', loadChildren: () => import('../../modules/settings/page-right-access/page-right-access.module').then(m => m.PageRightAccessModule), data: { breadcrumb: [{ title: 'Page Right Access', active: true }] }  },
-  { path: 'agency-registration', loadChildren: () => import('../../modules/masters/agency-registration/agency-registration.module').then(m => m.AgencyRegistrationModule) },
-  { path: 'language-setting', loadChildren: () => import('../../modules/settings/language-setting/language-setting.module').then(m => m.LanguageSettingModule) },
-  { path: 'performance-indicators', loadChildren: () => import('../../modules/settings/performance-indicators/performance-indicators.module').then(m => m.PerformanceIndicatorsModule) },
+  { path: 'dashboard', loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule), data: { breadcrumb: [{ title: 'Dashboard', active: true }] }, canActivate: [ExpenseGuard] },
+  { path: 'designation-master', loadChildren: () => import('../../modules/masters/designation-master/designation-master.module').then(m => m.DesignationMasterModule), data: { breadcrumb: [{ title: 'Designation Master', active: true }] } , canActivate: [ExpenseGuard] },
+  { path: 'school-registration', loadChildren: () => import('../../modules/masters/school-registration/school-registration.module').then(m => m.SchoolRegistrationModule), data: { breadcrumb: [{ title: 'School Registration', active: true }] }, canActivate: [ExpenseGuard]   },
+  { path: 'student-registration', loadChildren: () => import('../../modules/masters/student-registration/student-registration.module').then(m => m.StudentRegistrationModule), data: { breadcrumb: [{ title: 'Student Registration', active: true }] }, canActivate: [ExpenseGuard]  },
+  { path: 'user-registration', loadChildren: () => import('../../modules/masters/user-registration/user-registration.module').then(m => m.UserRegistrationModule), data: { breadcrumb: [{ title: 'User Registration', active: true }] }, canActivate: [ExpenseGuard] },
+  { path: 'page-right-access', loadChildren: () => import('../../modules/settings/page-right-access/page-right-access.module').then(m => m.PageRightAccessModule), data: { breadcrumb: [{ title: 'Page Right Access', active: true }] }, canActivate: [ExpenseGuard] },
+  { path: 'agency-registration', loadChildren: () => import('../../modules/masters/agency-registration/agency-registration.module').then(m => m.AgencyRegistrationModule), data: { breadcrumb: [{ title: 'Agency Registration', active: true }] }, canActivate: [ExpenseGuard] },
+  { path: 'language-setting', loadChildren: () => import('../../modules/settings/language-setting/language-setting.module').then(m => m.LanguageSettingModule), data: { breadcrumb: [{ title: 'Page Right Access', active: true }] }, canActivate: [ExpenseGuard] },
+  { path: 'performance-indicators', loadChildren: () => import('../../modules/settings/performance-indicators/performance-indicators.module').then(m => m.PerformanceIndicatorsModule), data: { breadcrumb: [{ title: 'Performance Indicators', active: true }] }, canActivate: [ExpenseGuard] },
   
 ];
 
