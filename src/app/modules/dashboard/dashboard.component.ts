f87ec18772ccfecf1570d78b80083f4b3d3ec64f
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2022-12-23 10:15:30 +0530

    take pull

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +7,9 @@ import { CommonMethodsService } from 'src/app/core/services/common-methods.servi
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-constructor(public translate:TranslateService,
-  private common:CommonMethodsService){}
+constructor(public translate:TranslateService){}
 
 ngOnInit(){
-  console.log("localStrorage Data",this.common.getLocalStorageData());
-  let data=this.common.getLocalStorageData();
-  console.log(JSON.parse(data).responseData);
+
 }
 }
