75988d4d81f152dbc2f326c30b0159da1f18abf2
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2023-01-11 16:30:40 +0530

    change page url and title

--- src/app/modules/masters/designation-master/designation-master.component.ts
+++ src/app/modules/masters/designation-master/designation-master.component.ts
@@ -9,6 +9,7 @@ import { ExcelPdfDownloadService } from 'src/app/core/services/excel-pdf-downloa
 import { MasterService } from 'src/app/core/services/master.service';
 import { WebStorageService } from 'src/app/core/services/web-storage.service';
 import { GlobalDialogComponent } from 'src/app/shared/components/global-dialog/global-dialog.component';
+import { ViewDialogComponent } from 'src/app/shared/components/view-dialog/view-dialog.component';
 import { AddDesignationComponent } from './add-designation/add-designation.component';
 
 @Component({
@@ -143,6 +144,7 @@ export class DesignationMasterComponent {
         this.globalDialogOpen(obj);
         break;
       case 'Row':
+      this.viewDataDialog(obj);
         break;
     }
   }
@@ -224,4 +226,22 @@ export class DesignationMasterComponent {
     this.excelPdf.downloadExcel(this.tableDataArray, pageName, header, column);
   }
 
+  viewDataDialog(obj:any){   //view table data
+    console.log(obj)
+    let linkedToData;
+    obj.newLinkedToDesignationName.forEach((ele:any) => {
+      linkedToData.push(ele)
+    });
+    let viewObj = {
+      cardTitle: this.lang == 'mr-IN' ? '' : 'Designation Master',
+      label:['Designation Name','Designation Level','Linked To'],
+      value:[obj.designationName,obj.designationLevelName,obj.linkedToData]
+    }
+     this.dialog.open(ViewDialogComponent, {
+      width: '850px',
+      data: viewObj,
+      disableClose: true,
+      autoFocus: false
+    })
+  }
 }
---
+++ src/app/shared/components/view-dialog/view-dialog.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-view-dialog',
+  templateUrl: './view-dialog.component.html',
+  styleUrls: ['./view-dialog.component.scss'],
+  standalone: true,
+  imports:[CommonModule,TranslateModule,MatCardModule,MatIconModule,MatDialogModule,MatButtonModule ]
+})
+export class ViewDialogComponent {
+
+constructor(
+  public dialogRef: MatDialogRef<ViewDialogComponent>,
+  @Inject(MAT_DIALOG_DATA) public data: any){}
+
+  ngOnInit(){
+    console.log("data",this.data);
+  }
+}
