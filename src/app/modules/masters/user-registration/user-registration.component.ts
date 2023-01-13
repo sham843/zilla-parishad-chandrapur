eb946f8a579db002d8300c7631e825df46feef12
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2023-01-13 19:40:28 +0530

    changes in excel

--- src/app/modules/masters/user-registration/user-registration.component.ts
+++ src/app/modules/masters/user-registration/user-registration.component.ts
@@ -261,9 +261,9 @@ setTableData(){     // table
     let pageName:any;
     this.lang=='mr-IN'?pageName='वापरकर्ता नोंदणी':pageName='User Registration';
     let header:any;
-    this.lang=='mr-IN'?header=['अनुक्रमांक','नाव','वापरकर्ता प्रकार ','पातळी','पदनाव','मोबाईल नंबर']:header=['Sr. No.', 'Name','User Type','Level','Designation', 'Mobile No'];
+    this.lang=='mr-IN'?header=['अनुक्रमांक','नाव','वापरकर्ता प्रकार ','पातळी','पदनाव','मोबाईल नंबर','ब्लॉक स्थिती']:header=['Sr. No.', 'Name','User Type','Level','Designation', 'Mobile No','Block Status'];
     let column:any;
-    this.lang=='mr-IN' && this.apiService.translateLang?column=['srNo','name','m_UserType','m_DesignationLevel','m_DesignationName','mobileNo']:column=['srNo', 'name','userType','designationLevel', 'designationName', 'mobileNo'];
+    this.lang=='mr-IN' && this.apiService.translateLang?column=['srNo','name','m_UserType','m_DesignationLevel','m_DesignationName','mobileNo','isBlock']:column=['srNo', 'name','userType','designationLevel', 'designationName', 'mobileNo','isBlock'];
     this.excelDowobj ={'pageName':pageName,'header':header,'column':column}
   }
   //#endregion------------------------------------------------End download pdf and excel method-----------------------------------------
