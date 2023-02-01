e5444c298864a9d3bd81e8f77fd947eb540757b3
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-02-01 14:32:26 +0530

    changes in inspection-report page

--- src/app/modules/reports/inspection-report/inspection-report.module.ts
+++ src/app/modules/reports/inspection-report/inspection-report.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { InspectionReportRoutingModule } from './inspection-report-routing.module';
 import { InspectionReportComponent } from './inspection-report.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { TableGridComponent } from 'src/app/shared/components/table-grid/table-grid.component';
+import { ViewDialogComponent } from 'src/app/shared/components/view-dialog/view-dialog.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +22,19 @@ import { InspectionReportComponent } from './inspection-report.component';
   ],
   imports: [
     CommonModule,
-    InspectionReportRoutingModule
+    InspectionReportRoutingModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TableGridComponent,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    NgApexchartsModule,
+    MatButtonModule,
+    ViewDialogComponent,
+    MatCheckboxModule,
+    MatIconModule
   ]
 })
 export class InspectionReportModule { }
